e3fa309c574e3930441cf3b07f7341bbccd8b67b
Zafarbek <madreimovzafarbek2@gmail.com>
2024-07-11 00:31:29 +0500

    prime-typescript

--- src/components/contract.tsx
+++ src/components/contract.tsx
@@ -4,21 +4,27 @@ import Button from "./button";
 
 const Contract=()=> {
     return (
-     <section id="product" className={`${layout.section}`}>
-   <div className={`${layout.sectionInfo}`}> 
-     <h2 className={`${styles.heading2}`}>Bir necha oson qadamda <br className="sm:block hidden" />
-     kontrakt tuzish</h2>
-     <p className={`${styles.paragraph} max-w-[550px] mt-5`}>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid quaerat, possimus cumque corporis 
-      quam neque in labore repellendus. Culpa dolorum nesciunt, 
-      aut quia corporis dignissimos adipisci est facilis doloremque exercitationem?
-     </p>
-     <Button styles={"mt-10"}/>
-   </div>
-   <div className={`${layout.sectionImage}`}>  
-   <img src={card} alt="card" className="w-[100%] h-[100%]"/>
-   </div>
-     </section>
+      <section id='product' className={layout.section}>
+			<div className={layout.sectionInfo}>
+				<h2 className={styles.heading2}>
+					Bir necha oson <br className='sm:block hidden' /> qadamda
+					kontrakt tuzish
+				</h2>
+				<p className={`${styles.paragraph} max-w-[550px] mt-5`}>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Similique natus tempore a iusto laborum dolorem alias
+					repellendus veritatis corrupti optio!
+				</p>
+				<Button styles={'mt-10'} />
+			</div>
+			<div className={layout.sectionImage}>
+				<img
+					src={card}
+					alt='card '
+					className='w-[100%] h-[100%]'
+				/>
+			</div>
+		</section>
     )
 }
 export default Contract;
